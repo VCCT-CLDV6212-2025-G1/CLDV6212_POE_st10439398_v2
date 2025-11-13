f8838a7848488ff93bc1ef5165a62dd39f8af927
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-14 00:05:33 +0200

    Added AccountController For login logic

---
+++ CLDV6212_POE_st10439398/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CLDV6212_POE_st10439398.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
